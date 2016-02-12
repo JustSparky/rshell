bc1d670d32dafca991a980d0ffce767d562eeb6e
Alex Tran <atran049@hammer.cs.ucr.edu>
2016-02-12 14:10:35 -0800

    Updated rshell.cpp and removed command cpp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -3,22 +3,48 @@
 class rshell{
     protected:
         string commmands;
-        vector<string> commandlist;
+        vector<string*> commandlist;
+	bool prevCommandPass;
     public:
         rshell(){
-		commands = NULL;
+	    commands = NULL;
         }
 	void parseAllCommands(string com){
             char_separator<char> delims(" ");
 	    tokenizer(char_separator<char>> tokenlist(com, delims);
             for (tokenizer<char_separator<char>>::iterator i = tokenlist.begin(); i != tokenlist.end(); i++){
-	        std::string command(*i);
-		commandlist.push_back(command);
+	        std:string com(*i);
+		commandlist.push_back(com);
             }
-       }
-       int executeCommand(){
+        }
+        void createParsed(){
+            for(int i = 0; i < commandlist.size(); i++){
+	        if(commandlist.at(i) != ";"){
+                    
+        int executeCommand(){
             char* array = new char[
 	    execvp(array0,array)
 	    p
         }
-
+	bool checkBreaker(string str){
+	    if (str == "||" || str == "&&" || str == ";"){
+	        return true;
+            }
+	}
+	bool checkCommandRun(string str){
+	    if (str == "||"){
+		if(prevCommandPass){
+	            return false;
+		}
+		return true;
+	    }
+	    if (str == "&&"){
+		if(prevCommandPass){
+		    return true;
+		}
+		return false;
+	    }
+	    if (str == ";"){
+		return true;
+	    }
+	}
