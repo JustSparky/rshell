a9d52e936980d7af6a21c0edb3e20444800ce963
Alex Tran <atran049@hammer.cs.ucr.edu>
2016-03-04 23:25:43 -0800

    Fixed spacing

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -12,7 +12,7 @@ using namespace std;
 class rshell{
 	protected:
 		//Data Members
-       		string commands;
+		string commands;
 		string nextConnector;
 		vector<string> commandlist;
 		bool prevCommandPass;
@@ -20,35 +20,35 @@ class rshell{
 		bool forceExit;
 	public:
 		//Constructor
-        	rshell(){
-	    		commands = "";
-	    		nextConnector = ";";
-	    		prevCommandPass = true;
-	    		forceExit = false;
+		rshell(){
+			commands = "";
+			nextConnector = ";";
+			prevCommandPass = true;
+			forceExit = false;
 			allCount = true;
 		}
 		// Parses the string (strings ending in ';' will keep the ';')
 		void parseAllCommands(){
 			char_separator<char> delims(" ");
-    			tokenizer<char_separator<char> > tokenlist(commands, delims);
-		       	for (tokenizer<char_separator<char> >::iterator i = tokenlist.begin(); i != tokenlist.end(); i++){
-		    		string com(*i);
+			tokenizer<char_separator<char> > tokenlist(commands, delims);
+			for (tokenizer<char_separator<char> >::iterator i = tokenlist.begin(); i != tokenlist.end(); i++){
+				string com(*i);
 				commandlist.push_back(com);
-        		}
-    		}
+			}
+		}
 
 		// Executes one command and sets prevCommandPass to true or false.
 		void executeCommand(vector<string> com){
-	    		char* argv[1024];
-	    		pid_t p = getpid();
-	    		pid_t pid = fork();
-             		int status;
+			char* argv[1024];
+			pid_t p = getpid();
+			pid_t pid = fork();
+			int status;
 
-        		for(unsigned int i = 0; i < com.size(); i++){
-	        		argv[i] = (char*)com.at(i).c_str();
-        		}
-	    		argv[com.size()] = NULL;
-			
+			for(unsigned int i = 0; i < com.size(); i++){
+				argv[i] = (char*)com.at(i).c_str();
+			}
+			argv[com.size()] = NULL;
+
 			if (pid == 0){
 				if (execvp(argv[0], argv)){
 					prevCommandPass = true;
@@ -68,44 +68,43 @@ class rshell{
 				perror("fork failed: ");
 				_exit(1);
 			}
-
 		}	
 
 		//Splits commandlist into commands with their arguments then calls executeCommand to run them.
 		void executeAllCommands(){
-       			vector<string> commandsublist;
-    			unsigned int i = 0;
-    			unsigned int j = 0;
-    			while (i < commandlist.size()){
+			vector<string> commandsublist;
+			unsigned int i = 0;
+			unsigned int j = 0;
+			while (i < commandlist.size()){
 				j = 0;
 				if (checkCommandRun()){
-        				while (!checkBreaker(commandlist.at(i))){
+					while (!checkBreaker(commandlist.at(i))){
 						//Exit check
-       	        				if (commandlist.at(i) == "exit"){
+						if (commandlist.at(i) == "exit"){
 							cout << "Forced Exit." << endl;
-                   					forceExit = true;
-	        					_Exit(0);
-	        				}
+							forceExit = true;
+							_Exit(0);
+						}
 						// Comment check
-            					if (commandlist.at(i) == "#" || checkComment(commandlist.at(i))){
-		        				executeCommand(commandsublist);
-		        				return;
-		        			}
+						if (commandlist.at(i) == "#" || checkComment(commandlist.at(i))){
+							executeCommand(commandsublist);
+							return;
+						}
 						//Adds command to the list
-	            				commandsublist.push_back(commandlist.at(i));
+						commandsublist.push_back(commandlist.at(i));
 						if (commandsublist.at(j).at(commandsublist.at(j).length() - 1) == ';'){
 							commandsublist.at(j).erase(commandsublist.at(j).length() - 1);
 							nextConnector = ';';
 							break;
 						}
-		        			i++;
+						i++;
 						j++;
 						if (i == commandlist.size()){
 							executeCommand(commandsublist);
 							return;
 						}		
-                			}		
-	                		if (checkBreaker(commandlist.at(i))){
+					}		
+					if (checkBreaker(commandlist.at(i))){
 						if (nextConnector == "||"){
 							if (allCount == true){
 								prevCommandPass = true;
@@ -130,8 +129,8 @@ class rshell{
 							allCount = true;
 							prevCommandPass = true;
 						}
-   		 			    	nextConnector = commandlist.at(i);
-			    		}
+						nextConnector = commandlist.at(i);
+					}
 					i++;
 				}
 				executeCommand(commandsublist);
@@ -144,13 +143,13 @@ class rshell{
 			if (str.at(0) == '#'){
 				return true;
 			}
-			return false;
+			return false
 		}
-
+		
 		//	Checks if the string is a breaker
 		bool checkBreaker(string str){
 			if (str == "||" || str == "&&" || str == ";"){
-		        	return true;
+				return true;
 			}
 			return false;
 		}
@@ -159,15 +158,15 @@ class rshell{
 		bool checkCommandRun(){
 			if (nextConnector == "||"){
 				if(allCount == true){
-		        		return false;
+					return false;
 				}
 				else{
 					return true;
 				}
-		    	}
+			}
 			if (nextConnector == "&&"){
 				if(allCount == true){
-				    	return true;
+					return true;
 				}
 				cout << "TESTED FALSE" << endl;
 				return false;
@@ -182,9 +181,9 @@ class rshell{
 		void run(){
 			while (!forceExit && commands != "exit"){
 				cout << "$";
-			    	getline(cin, commands);
-			    	parseAllCommands();
-  	 		     	executeAllCommands();
+				getline(cin, commands);
+				parseAllCommands();
+				executeAllCommands();
 				commandlist.clear();
 				nextConnector = ";";
 				prevCommandPass = true;	
