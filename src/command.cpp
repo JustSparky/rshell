8b39dd68aa25bf9ce15f2a9883351581520181b9
Alex Tran <atran049@hammer.cs.ucr.edu>
2016-03-06 14:11:38 -0800

    Created Command Class

---
+++ src/command.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <cstdio>
+#include <sys/wait.h>
+
+using namespace std;
+
+class command{
+	protected:
+		vector<string> commandlist;
+		bool commandPass;
+
+};
