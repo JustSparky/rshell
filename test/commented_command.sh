b329496306f067feb243ecacac3dafa756134c39
Kevin Tran <ktran050@hammer.cs.ucr.edu>
2016-03-06 21:54:17 -0800

    comment test script

--- test/commented_command.sh
+++ test/commented_command.sh
@@ -1,8 +1,28 @@
+
 echo "Test with ls and comment: "
-    ls #hello
-echo "Test with ls and comment followed by commented out ls: "
-    ls #hello;
+	./../bin/rshell << EOF
+	ls #hello
+	exit
+EOF
 echo "Test with multiple comments: "
+	./..bin/rshell << EOF
     ls #eee#eee #eee
+	exit
+EOF
 echo "Test with comments and || and &&: "
+	./../bin/rshell << EOF
     ls && ls #ls || ls
+	exit
+EOF
+
+echo "Test with test"
+	./../bin/rshell << EOF
+	test #test_test.sh
+	exit
+EOF
+
+echo "Test with precedence"
+	./..bin/rshell << EOF
+	(test test_test.sh) && ls -a
+	exit
+EOF
