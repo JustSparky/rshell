061c8c0384f69cc25cee4de8bc879f21368a4708
Alex Tran <atran049@hammer.cs.ucr.edu>
2016-03-06 21:42:16 -0800

    edited test test

--- test/test_test.sh
+++ test/test_test.sh
@@ -1,7 +1,17 @@
 echo "Test with test command.cpp"
-	cat test command.cpp
+	./../bin/rshell << EOF
+test single_command.sh
+exit
+EOF
+
 echo "Test with command.cpp in brackets"
-	cat [command.cpp]
+	./../bin/rshell << EOF
+[command.cpp]
+exit
+EOF
 echo "Test for non existant directory"
-	cat test -d fakedir
+	./../bin/rshell << EOF
+test -d fakedir
+exit
+EOF
 
