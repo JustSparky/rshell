5cd2131d8b86c9368eb5ac0e25d052822c362978
Alex Tran <atran049@hammer.cs.ucr.edu>
2016-03-06 21:56:25 -0800

    Removed spacing

--- test/commented_command.sh
+++ test/commented_command.sh
@@ -1,28 +1,28 @@
 
 echo "Test with ls and comment: "
 	./../bin/rshell << EOF
-	ls #hello
-	exit
+ls #hello
+exit
 EOF
 echo "Test with multiple comments: "
 	./..bin/rshell << EOF
-    ls #eee#eee #eee
-	exit
+ls #eee#eee #eee
+exit
 EOF
 echo "Test with comments and || and &&: "
 	./../bin/rshell << EOF
-    ls && ls #ls || ls
-	exit
+ls && ls #ls || ls
+exit
 EOF
 
 echo "Test with test"
 	./../bin/rshell << EOF
-	test #test_test.sh
-	exit
+test #test_test.sh
+exit
 EOF
 
 echo "Test with precedence"
-	./..bin/rshell << EOF
-	(test test_test.sh) && ls -a
-	exit
+	./../bin/rshell << EOF
+(test test_test.sh) && ls -a
+exit
 EOF
