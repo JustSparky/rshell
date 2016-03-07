4e7d57c5b495ed20cd8db33d0b38594f87112375
Alex Tran <atran049@hammer.cs.ucr.edu>
2016-03-06 21:12:44 -0800

    Added test files

---
+++ test/test_test.sh
@@ -0,0 +1,7 @@
+echo "Test with test command.cpp"
+	cat test command.cpp
+echo "Test with command.cpp in brackets"
+	cat [command.cpp]
+echo "Test for non existant directory"
+	cat test -d fakedir
+
